7adb2bbc1ecf57194db62f8879242a83ec3a01a9
Elisa <elisaharimalala07@gmail.com>
2026-06-16 05:47:02 +0300

    changement

---
+++ deploy.bat
@@ -0,0 +1,35 @@
+@echo off
+:: Définition des variables
+set APP_NAME=framWorkPerso
+set SRC_DIR=src\main\java
+set WEB_DIR=src\main\webapp
+set BUILD_DIR=build
+set LIB_DIR=lib
+set TOMCAT_WEBAPPS=D:\xampp\tomcat\webapps
+set SERVLET_API_JAR=%LIB_DIR%\servlet-api.jar
+
+:: Nettoyage et création des répertoires temporaires
+if exist %BUILD_DIR% rmdir /s /q %BUILD_DIR%
+mkdir %BUILD_DIR%\WEB-INF\classes
+
+:: Compilation des fichiers Java
+:: Sous Windows CMD, on liste les fichiers .java dans un fichier temporaire
+dir /s /B %SRC_DIR%\*.java > sources.txt
+javac -cp "%SERVLET_API_JAR%" -d %BUILD_DIR%\WEB-INF\classes @sources.txt
+del sources.txt
+
+:: Copier les fichiers web (web.xml, JSP, etc.)
+xcopy /E /I /Y %WEB_DIR%\* %BUILD_DIR%\
+
+:: Générer le fichier .war dans le dossier build
+cd %BUILD_DIR%
+jar -cvf %APP_NAME%.war *
+cd ..
+
+:: Déploiement dans Tomcat
+copy /Y %BUILD_DIR%\%APP_NAME%.war "%TOMCAT_WEBAPPS%\"
+
+echo.
+echo Deploiement termine. Redemarrez Tomcat si necessaire.
+echo.
+pause
