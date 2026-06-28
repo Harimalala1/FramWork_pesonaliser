93c17f1f39e6511a777679dc54e699f90aa0b131
Elisa <elisaharimalala07@gmail.com>
2026-06-28 22:40:21 +0300

    ajout fonction sprint2

---
+++ src/main/java/main/java/annotation/GetMapping.java
@@ -0,0 +1,9 @@
+package framework.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface GetMapping {
+    String value();
+}
